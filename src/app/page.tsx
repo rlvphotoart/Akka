1a95e19e857228b9278e548a65c38bc443a9f29c
Radu Luca Victor <rlvphotoart@gmail.com>
2025-05-07 21:53:20 +0300

    Fix TypeScript errors and update Image components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,7 +59,6 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <Image
-            aria-hidden
             src="/file.svg"
             alt="File icon"
             width={16}
@@ -74,7 +73,6 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <Image
-            aria-hidden
             src="/window.svg"
             alt="Window icon"
             width={16}
@@ -89,7 +87,6 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <Image
-            aria-hidden
             src="/globe.svg"
             alt="Globe icon"
             width={16}
